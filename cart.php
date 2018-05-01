2df5d0ba15d1a44554b090435f2ca50ef43600fe
unknown <hfco1995@gmail.com>
2018-05-01 00:34:26 -0500

    Version Base para añadir cosas

---
+++ cart.php
@@ -0,0 +1,109 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Yachay Market</title>
+		<link rel="stylesheet" href="css/bootstrap.min.css"/>
+		<script src="js/jquery2.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="main.js"></script>
+		<link rel="stylesheet" type="text/css" href="style.css"/>
+	</head>
+<body>
+<div class="wait overlay">
+	<div class="loader"></div>
+</div>
+	<div class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container-fluid">	
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapse" aria-expanded="false">
+					<span class="sr-only">navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a href="#" class="navbar-brand">Yachay Market</a>
+			</div>
+		<div class="collapse navbar-collapse" id="collapse">
+			<ul class="nav navbar-nav">
+				<li><a href="index.php"><span class="glyphicon glyphicon-home"></span>   Inicio</a></li>
+				<li><a href="index.php"><span class="glyphicon glyphicon-modal-window"></span>   Productos</a></li>
+			</ul>
+		</div>
+	</div>
+	</div>
+	<p><br/></p>
+	<p><br/></p>
+	<p><br/></p>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-2"></div>
+			<div class="col-md-8" id="cart_msg">
+				<!--Cart Message--> 
+			</div>
+			<div class="col-md-2"></div>
+		</div>
+		<div class="row">
+			<div class="col-md-2"></div>
+			<div class="col-md-8">
+				<div class="panel panel-primary">
+					<div class="panel-heading">Revise su carrito</div>
+					<div class="panel-body">
+						<div class="row">
+							<div class="col-md-2 col-xs-2"><b>Eliminar/Actualizar</b></div>
+							<div class="col-md-2 col-xs-2"><b>Imagen del producto</b></div>
+							<div class="col-md-2 col-xs-2"><b>Nombre Producto</b></div>
+							<div class="col-md-2 col-xs-2"><b>Cantidad</b></div>
+							<div class="col-md-2 col-xs-2"><b>Precio Unitario</b></div>
+							<div class="col-md-2 col-xs-2"><b>Precio Total $</b></div>
+						</div>
+						<div id="cart_checkout"></div>
+						<!--<div class="row">
+							<div class="col-md-2">
+								<div class="btn-group">
+									<a href="#" class="btn btn-danger"><span class="glyphicon glyphicon-trash"></span></a>
+									<a href="" class="btn btn-primary"><span class="glyphicon glyphicon-ok-sign"></span></a>
+								</div>
+							</div>
+							<div class="col-md-2"><img src='product_images/imges.jpg'></div>
+							<div class="col-md-2">Product Name</div>
+							<div class="col-md-2"><input type='text' class='form-control' value='1' ></div>
+							<div class="col-md-2"><input type='text' class='form-control' value='5000' disabled></div>
+							<div class="col-md-2"><input type='text' class='form-control' value='5000' disabled></div>
+						</div> -->
+						<!--<div class="row">
+							<div class="col-md-8"></div>
+							<div class="col-md-4">
+								<b>Total $500000</b>
+							</div> -->
+						</div> 
+					</div>
+					<div class="panel-footer"></div>
+				</div>
+			</div>
+			<div class="col-md-2"></div>
+			
+		</div>
+</body>	
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		
